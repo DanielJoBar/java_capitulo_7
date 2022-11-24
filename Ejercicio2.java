1c78ca77e063872090f5693100e5c99de83b45b2
DanielJoBar <jodar.barbero.daniel@gmail.com>
2022-11-24 09:50:49 +0100

    done

---
+++ Ejercicio2.java
@@ -0,0 +1,15 @@
+public class Ejercicio2{
+    public static void main(String[] args){
+        String[] caracteres= new String[10];
+        caracteres[0]="'a'";
+        caracteres[1]="'x'";
+        caracteres[4]="'@'";
+        caracteres[6]="' '";
+        caracteres[7]="'+'";
+        caracteres[8]="'Q'";
+        for(int i=0;i<=9;i++){
+            System.out.println("( "+caracteres[i]+" )");
+        }
+
+    }
+}
