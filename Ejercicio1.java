d3e742fe34ef4c851f830930e6dfe9d50f3d8d80
DanielJoBar <jodar.barbero.daniel@gmail.com>
2022-11-24 09:39:27 +0100

    done

---
+++ Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1{
+    public static void main(String[] args){
+        int[] num = new int[12];
+        num[0]=39;
+        num[1]=-2;
+        num[5]=0;
+        num[7]=14;
+        num[9]=5;
+        num[10]=120;
+        for(int i=0;i<=12;i++)
+        System.out.printf("( %d )\n",num[i]);
+
+    }
+}
